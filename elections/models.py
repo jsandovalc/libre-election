c11f325aa8edbca88cfbb5f7e099ebea4c178a04
Jonathan Sandoval <jsandoval@utp.edu.co>
2017-09-21 15:31:20 -0500

    Add Election model.
    An Election is valid only from start_date to end_date.

--- elections/models.py
+++ elections/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Election(models.Model):
+    """Almost everty other model belongs to an Election."""
+
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
