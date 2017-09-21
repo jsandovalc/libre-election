2da57b04e7fb6ecf1a9b47267deef49a0514086f
Jonathan Sandoval <jsandoval@utp.edu.co>
2017-09-21 15:34:07 -0500

    Add Election model.

--- libre_election/settings.py
+++ libre_election/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'elections.apps.ElectionsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
