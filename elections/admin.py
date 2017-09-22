5ea171bcc3d4b4e69d1063bae749e9a67d3ca2c9
Jonathan Sandoval <jsandoval@utp.edu.co>
2017-09-22 18:30:57 -0500

    Add voters to app.

--- elections/admin.py
+++ elections/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Election, List
+from .models import Election, List, Voter
 
 
 admin.site.register(Election)
 admin.site.register(List)
+admin.site.register(Voter)
--- elections/models.py
+++ elections/models.py
@@ -10,6 +10,14 @@ class Election(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
 
+    class Meta:
+        verbose_name = "Elección"
+        verbose_name_plural = "Elecciones"
+
+
+    def __str__(self):
+        return f'Elección: {self.name}'
+
 
 class List(models.Model):
     """A list of candidates."""
@@ -17,3 +25,24 @@ class List(models.Model):
     description = models.TextField()
 
     election = models.ForeignKey('Election', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Lista"
+        verbose_name_plural = "Listas"
+
+    def __str__(self):
+        return f"Lista: {self.short_description}"
+
+
+class Voter(models.Model):
+    """A voter for an election."""
+    document = models.CharField(max_length=20)
+
+    election = models.ForeignKey('Election', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Votante"
+        verbose_name_plural = "Votantes"
+
+    def __str__(self):
+        return f"Votante: {self.document}"
