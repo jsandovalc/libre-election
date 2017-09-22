30bd33b234042cae8accad3e208a8258c28b4349
Jonathan Sandoval <jsandoval@utp.edu.co>
2017-09-22 18:12:07 -0500

    Add list model.

--- elections/models.py
+++ elections/models.py
@@ -9,3 +9,11 @@ class Election(models.Model):
 
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
+
+
+class List(models.Model):
+    """A list of candidates."""
+    short_description = models.CharField(max_length=50)
+    description = models.TextField()
+
+    election = models.ForeignKey('Election', on_delete=models.CASCADE)
