e6afe7c0c78a3c0ee0d3917d8a5bae981a56c617
Jonathan Sandoval <jsandoval@utp.edu.co>
2017-09-22 19:11:12 -0500

    Add voting jury to admin.

--- elections/admin.py
+++ elections/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Election, List, Voter, PollingStation
+from .models import Election, List, Voter, PollingStation, VotingJury
 
 
 admin.site.register(Election)
 admin.site.register(List)
 admin.site.register(Voter)
 admin.site.register(PollingStation)
+admin.site.register(VotingJury)
