6e76401b0427ecb74a6e4144d88292375bb2e261
Jonathan Sandoval <jsandoval@utp.edu.co>
2017-09-25 17:02:20 -0500

    Add url por poll.

--- elections/urls.py
+++ elections/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.Index.as_view(), name='index'),
+    url(r'^poll/(?P<document>\d+)$', views.Poll.as_view(), name='poll'),
 ]
