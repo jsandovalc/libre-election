f04941c371ca60ba0aaa44e2620df6165a76e9a2
Jonathan Sandoval <jsandoval@utp.edu.co>
2017-09-25 17:02:07 -0500

    Add poll view.
    - Index is now a class based view.

--- elections/views.py
+++ elections/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Voter, VotingJury
 
 
-@login_required
-def index(request):
-    return render(request, 'index.html')
+class Index(LoginRequiredMixin, View):
+    """Form to login with document. No password required."""
+    def get(self, request):
+        return render(request, 'index.html')
+
+    def post(self, request):
+        document = request.POST.get('document')
+
+        try:
+            voter = Voter.objects.get(
+                document=document,
+                election=VotingJury.objects.get(user=self.request.user).polling_station.election)
+        except Voter.DoesNotExist:
+            context = dict(message=f'La cédula {document} no está registrada para esta elección')
+            return render(request, 'index.html',
+                          context=context)
+
+        return redirect('poll', document=document)
+
+
+class Poll(LoginRequiredMixin, View):
+    """View to vote."""
+    def get(self, request, document):
+        return render(request, 'poll.html')
